fff17c9ebe3e0debf1f9c55f09ba9c48e8408a30
Alex Borisovec <yadyadyastrannik85@gmail.com>
2024-10-28 22:52:09 +0300

    [add] text field and delegat

--- hacker/Login2/Login2ViewController.swift
+++ hacker/Login2/Login2ViewController.swift
@@ -11,18 +11,36 @@ class Login2ViewController: UIViewController {
         super.viewDidLoad()
         setupSUbiews()    // Выстраиваем иерархию сабвьюх
         setupLayout()    // Задаем верстку
-        
+        view.backgroundColor = .white
         
         buttonLogin.addAction(UIAction(handler: { _ in
-            
         }), for: .touchUpInside)
+        
+        
     }
+    private lazy var textFieldLogin: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Login"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        textField.delegate = self
+        return textField
+    }()
+    
+    private lazy var textFieldPassword: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        textField.delegate = self
+        return textField
+    }()
     
     private let buttonLogin: UIButton = {   // создаем кнопку кодом
         let button = UIButton()
         button.setTitle("Login", for: .normal)
-        button.setTitleColor(.red, for: .normal)
-        button.setTitleColor(.blue, for: .highlighted)
+        button.setTitleColor(.white, for: .normal)
+        button.setTitleColor(.gray, for: .highlighted)
         button.backgroundColor = .blue
         button.layer.cornerRadius = 10
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -32,12 +50,27 @@ class Login2ViewController: UIViewController {
     // MARK: - Private
     
     private func setupSUbiews() {
+        view.addSubview(textFieldLogin)
+        view.addSubview(textFieldPassword)
         view.addSubview(buttonLogin)
     }
     
+
+    
     private func setupLayout() {   // настройка констрейна
         NSLayoutConstraint.activate([
-            buttonLogin.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),  // отступ сверху
+            textFieldLogin.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            textFieldLogin.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+            textFieldLogin.heightAnchor.constraint(equalToConstant: 50),
+            textFieldLogin.widthAnchor.constraint(equalToConstant: 300),
+            
+            textFieldPassword.topAnchor.constraint(equalTo: view.topAnchor, constant: 250),
+            textFieldPassword.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0),
+            textFieldPassword.heightAnchor.constraint(equalToConstant: 50),
+            textFieldPassword.widthAnchor.constraint(equalToConstant: 300),
+
+            
+            buttonLogin.topAnchor.constraint(equalTo: view.topAnchor, constant: 400),  // отступ сверху
     //        buttonLogin.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -200),  // отступ от низа писшем с минусом
     //        buttonLogin.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 150), // отступ от начала экрана
             buttonLogin.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 0), // Отцентрировали по горизонтали
@@ -45,6 +78,22 @@ class Login2ViewController: UIViewController {
             buttonLogin.widthAnchor.constraint(equalToConstant: 150)
             ])
     }
+  
     
     
 }
+
+
+extension Login2ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder() // Скрыть клавиатуру по нажатию на "Return"
+        if textField == textFieldLogin {
+            print("login")
+        } else if textField == textFieldPassword {
+            print("Password")
+        }
+        print("ВВеденный логин: \(textField.text ?? "")")
+        return true
+    }
+}
+
